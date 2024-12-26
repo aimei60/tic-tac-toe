873c063fe8e3e858bb7df3f1eddeef8f38141fcb
aimei60 <104590763+aimei60@users.noreply.github.com>
2024-12-26 22:12:09 +0000

    Sorted going into the game page from the player section and transferring the values into the game section under the playerToGame function

--- mainscript.js
+++ mainscript.js
@@ -42,42 +42,28 @@ function XandOPlayerDeclaration() {
     const player2O = document.querySelector(".player2-O-button")
 
     player1X.addEventListener('click', function() {
-        player1X.style.borderColor = "red";
+        player1X.classList.add("selected");
         player2X.disabled = true;
         player1O.disabled = true;
     })
 
     player1O.addEventListener('click', function() {
-        player1O.style.borderColor = "red";
+        player1O.classList.add("selected");
         player2O.disabled = true;
         player1X.disabled = true;
     })
 
     player2X.addEventListener('click', function() {
-        player2X.style.borderColor = "red";
+        player2X.classList.add("selected");
         player1X.disabled = true;
         player2O.disabled = true;
     })
 
     player2O.addEventListener('click', function() {
-        player2O.style.borderColor = "red";
+        player2O.classList.add("selected");
         player1O.disabled = true;
         player2X.disabled = true;
     })
-}
-
-/* captures the players name details for the game section */
-function capturePlayerDetails() {
-    const player1Input = document.querySelector(".Player-input1")
-    const player2Input = document.querySelector(".Player-input2")
-
-    if (player1Input.textContent == "") {
-        player1Input.textContent = "Player 1"
-    }
-
-    if (player2Input.textContent == "") {
-        player2Input.textContent = "Player 2"
-    }
 }
 
 /* start game into player section function and update player details on game page */
@@ -85,11 +71,55 @@ function playerToGame() {
     const submitButton = document.querySelector(".submit")
     const playerPage = document.querySelector(".player-selection-page")
     const gamePage = document.querySelector(".game-section")
+    const player1Details = document.querySelector(".Player-input1")
+    const player2Details = document.querySelector(".Player-input2")
+    const player1 = document.querySelector(".player1")
+    const player2 = document.querySelector(".player2")
+    const player1XButton = document.querySelector(".player1-X-button")
+    const player1OButton = document.querySelector(".player1-O-button")
+    const player2XButton = document.querySelector(".player2-X-button")
+    const player2OButton = document.querySelector(".player2-O-button")
+    const player1sign = document.querySelector(".player1sign")
+    const player2sign = document.querySelector(".player2sign")
+    const player1score = document.querySelector(".player1score")
+    const player2score = document.querySelector(".player2score")
+    let value = 0
+
 
     submitButton.addEventListener('click', function() {
         gamePage.style.display = 'block';
         playerPage.style.display = "none";
+
+        if (player1Details.value == "") {
+            player1.textContent = "Player 1"
+        } else {
+            player1.textContent = `Player 1: ${player1Details.value}`
+        }
+    
+        if (player2Details.value == "") {
+            player2.textContent = "Player 2"
+        } else {
+            player2.textContent = `Player 1: ${player2Details.value}`
+        }
+    
+
+        if (player1XButton.classList.contains("selected")) {
+            player1sign.textContent = `Player 1 Sign: ${player1XButton.textContent}`;
+        } else if (player1OButton.classList.contains("selected")) {
+            player1sign.textContent = `Player 1 Sign: ${player1OButton.textContent}`;
+        }
+
+        if (player2XButton.classList.contains("selected")) {
+            player2sign.textContent = `Player 2 Sign: ${player2XButton.textContent}`;
+        } else if (player2OButton.classList.contains("selected")) {
+            player2sign.textContent = `Player 2 Sign: ${player2OButton.textContent}`;
+        }
+
+        player1score.textContent = `Player 1 Score: ${value}`
+        player2score.textContent = `Player 2 Score: ${value}`
+
     })
+
 }
 
 
